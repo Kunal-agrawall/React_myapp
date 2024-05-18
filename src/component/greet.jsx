4edffe85618fb436f9a99143ba96b0b52fa4f78b
Kunal Agrawal <kunalgarg081@gmail.com>
2024-05-18 12:55:19 +0530

    init project

---
+++ src/component/greet.jsx
@@ -0,0 +1,10 @@
+
+const Greet = () => {
+  return (
+    <div className="bg-primary">
+      <h1>Radhe Radhe</h1>
+    </div>
+  )
+}
+
+export default Greet
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Greet from './component/greet'
+import Navbar from './component/navbar'
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>Jai Shri Radhe</h1>
+root.render(<>
 
+{/* <h1>Jai Shri Radhe</h1> */}
+<Greet/>
+<Greet/>
+<Navbar/>
+</>
 )
